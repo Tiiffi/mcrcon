7d5cb8016ee0b6a70019e7b2864f025676f43999
Tiiffi <tiiffi@gmail.com>
2019-10-21 14:23:41 +0300

    Change DATA_BUFFSIZE to 4096 bytes

--- mcrcon.c
+++ mcrcon.c
@@ -57,7 +57,7 @@
 #define RCON_PID                0xBADC0DE
 
 // a bit too big perhaps?
-#define DATA_BUFFSIZE 10240
+#define DATA_BUFFSIZE 4096
 
 // rcon packet structure
 typedef struct _rc_packet {
