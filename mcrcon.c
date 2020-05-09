8bd43ebb43d747a0e54573ba06b8eeacc8a54c0a
tmanifold <tdmanifold@gmail.com>
2020-05-09 00:06:10 -0400

    recommented _WIN32_WINNT define

--- mcrcon.c
+++ mcrcon.c
@@ -34,7 +34,7 @@
 #ifdef _WIN32
     // for name resolving on windows
     // enable this if you get compiler whine about getaddrinfo() on windows
-    #define _WIN32_WINNT 0x0501
+    // #define _WIN32_WINNT 0x0501
 
     #include <ws2tcpip.h>
     #include <winsock2.h>
